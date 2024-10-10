e80d769d029a472b29b72b9fb4456c3287109b20
sabrexghosh@gmail.com <96774134+Night3y3@users.noreply.github.com>
2024-10-10 14:45:26 +0530

    cors added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import dbConnect from "./db";
 import router from "./route";
+import cors from "cors";
 import "./lib/cronJob";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
